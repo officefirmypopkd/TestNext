699395ebf340502e84e2051b47b34e56e2c90320
FilipKubackiEmoLink <filip.kubacki@emo-link.com>
2024-08-14 15:27:23 +0200

    chore: test further 9

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,24 +1,25 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+// import { handleAuth } from '@auth0/nextjs-auth0';
 
-// import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+
 export const dynamic = 'force-dynamic';
 
-export const GET = handleAuth();
+// export const GET = handleAuth();
 
-// export const GET = handleAuth({
-//   login: handleLogin({
-//     authorizationParams: {
-//       audience: process.env.AUTH0_AUDIENCE,
-//       scope: 'openid profile email offline_access',
-//     },
-//     returnTo: '/profile',
-//   }),
-//   signup: handleLogin({
-//     authorizationParams: {
-//       screen_hint: 'signup',
-//       audience: process.env.AUTH0_AUDIENCE,
-//       scope: 'openid profile email offline_access',
-//     },
-//     returnTo: '/',
-//   }),
-// });
+export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+      audience: process.env.AUTH0_AUDIENCE,
+      scope: 'openid profile email offline_access',
+    },
+    returnTo: '/profile',
+  }),
+  signup: handleLogin({
+    authorizationParams: {
+      screen_hint: 'signup',
+      audience: process.env.AUTH0_AUDIENCE,
+      scope: 'openid profile email offline_access',
+    },
+    returnTo: '/profile',
+  }),
+});
